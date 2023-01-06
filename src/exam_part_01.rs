be09abe023ee150269a22ddbe53c3fd732ed1236
Gede Kitchen Batara <gedekitchenbatara21@gmail.com>
2023-01-06 17:23:03 +0800

    excercises 01 added menu to _print_info

--- src/exam_part_01.rs
+++ src/exam_part_01.rs
@@ -29,7 +29,7 @@ impl Termometer {
         return (self.fahrenheit - 32.00) * 5.00/9.00 + _DEFAULT_TERMOMETER_VALUE
     }
 
-    fn _fehrenheit_to_celsius(&self) -> f32 {
+    fn _fahrenheit_to_celcius(&self) -> f32 {
         return (self.fahrenheit - 32.00) * 5.00/9.00;
     }
 }
@@ -52,12 +52,27 @@ fn _input_float(message: &str) -> f32 {
 
 //print fungsi
 pub fn _print_info() {
-    let termomater_1 = Termometer {
-        fahrenheit: _input_float("Fahrenheit"),
-        celsius: _input_float("Celsius"),
-        kelvin: _input_float("Kelvin")
-    };
+    loop {
+        let termomater_1 = Termometer {
+            fahrenheit: _input_float("Fahrenheit"),
+            celsius: _input_float("Celsius"),
+            kelvin: _input_float("Kelvin")
+        };
 
-    println!("celsius {} to kelvin {}", termomater_1.celsius, termomater_1._celsius_to_kelvin());
-    println!("fahrenheit {}", termomater_1.fahrenheit);
+        let mut _input_num = String::new();
+        println!("Enter Converter menu please");
+        println!("1. Kelvin => Celsius\n2. Kelvin => Fahrenheit\n3. Celsius => Kelvin\n4. Celsius => Fahrenheit\n5. Fahrenheit => Kelvin\n6. Fahrenheit => Celsius");
+        io::stdin().read_line(&mut _input_num).expect("Cant read line");
+        let _input_menu: u32 = _input_num.trim().parse().expect("Input real menu please");
+
+        match _input_menu {
+            1 => return println!("Kelvin {} to Celsius {}", termomater_1.kelvin, termomater_1._kelvin_to_celsius()),
+            2 => return println!("Kelvin {} to Fahrenheit {}", termomater_1.kelvin, termomater_1._kelvin_to_fahrenheit()),
+            3 => return println!("Celsius {} to Kelvin {}", termomater_1.celsius, termomater_1._celsius_to_kelvin()),
+            4 => return println!("Celsius {} to Fahrenheit {}", termomater_1.celsius, termomater_1._celsius_to_fahrenheit()),
+            5 => return println!("Fahrenheit {} to Kelvin {}", termomater_1.fahrenheit, termomater_1._fahrenheit_to_kelvin()),
+            6 => return println!("Fahrenheit {} to Celsius {}", termomater_1.fahrenheit, termomater_1._fahrenheit_to_celcius()),
+            _ => continue
+        };
+    }
 }
